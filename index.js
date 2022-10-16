7688e0f8baf93424d1ebd1c2ecb62c6b044f5bad
Brayden <brayburl224@gmail.com>
2022-10-16 15:13:10 -0400

    shortened license selection to four main options

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const questions = [
         type: 'list',
         message: 'Select the license that you are using for your project. If there is no license, select "Unlicensed".',
         name: 'license',
-        choices: ['Apache 2.0', 'GNU General PUblic License 3.0', 'MIT License', 'BSD 2-Clause "simplified" License"', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'Unlicensed']
+        choices: ['Apache', 'MIT', 'GPLv3', 'Unlicensed']
     }
 ];
 
